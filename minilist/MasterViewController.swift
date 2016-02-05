ae4fd30f802e37595fbf2c7213592e74effea466
Hayley Cundiff <hcsparklegirl@gmail.com>
2016-02-05 08:56:34 -0700

    change name of new items

--- minilist/MasterViewController.swift
+++ minilist/MasterViewController.swift
@@ -38,7 +38,7 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
+        objects.insert(MiniList(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
@@ -48,7 +48,7 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row] as! MiniList
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -70,8 +70,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row] as! MiniList
+        cell.textLabel!.text = object.Ourname
         return cell
     }
 
---
+++ minilist/MiniList.swift
@@ -0,0 +1,16 @@
+//
+//  MiniList.swift
+//  minilist
+//
+//  Created by Hayley Cundiff on 2/5/16.
+//  Copyright © 2016 Artifylabs. All rights reserved.
+//
+
+import Foundation
+class MiniList {
+    var Ourname:String = "newitem"
+
+
+
+
+}
